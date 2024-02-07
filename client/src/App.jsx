21327a25b66eb1e866e0cb2920b0779f16c98b80
RajshekarPujari0 <rajshekarpujari2002@gmail.com>
2024-02-07 10:51:34 +0530

    Add Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-
-const App = () => {
-  return <div className="text-3xl font-bold underline">Hello Twitter</div>;
-};
+import { Route, Routes } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Home from "./pages/Home";
+import Messages from "./pages/Messages";
+import Lists from "./pages/Lists";
+import Communities from "./pages/Communities";
+import Profile from "./pages/Profile";
+import Premium from "./pages/Premium";
+ 
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/notifications" element={<Notification />} />
+      <Route path="/messages" element={<Messages />} />
+      <Route path="/lists" element={<Lists />} />
+      <Route path="/communities" element={<Communities />} />
+      <Route path="/Premium" element={<Premium />} />
+      <Route path="/Profile" element={<Profile />} />
+    </Routes>
+  );
+}
 
 export default App;
